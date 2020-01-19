69b80fdae2dc8934845f4f50de93b77e7e9c180f
andrevsl <andrevsl@gmail.com>
2020-01-19 16:29:04 -0300

    Blog_Portal Testemonials-Loop Carousel

--- src/app/shared/testemonials/testemonials.component.ts
+++ src/app/shared/testemonials/testemonials.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { NgbCarouselConfig, NgbCarousel, NgbSlideEvent, NgbSlideEventSource  } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ds-testemonials',
@@ -8,14 +8,24 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class TestemonialsComponent implements OnInit {
 
-  constructor(config:NgbCarouselConfig) {
+  /*constructor(config:NgbCarouselConfig) {
   config.interval = 5000;
   config.wrap = false;
   config.showNavigationIndicators=true;
   config.showNavigationArrows = false;
-  config.pauseOnHover = false; }
+  config.pauseOnHover = true; }*/
+    paused = false;
+    unpauseOnArrow = false;
+    pauseOnIndicator = false;
+    pauseOnHover = true;
+
+    @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+
+
+
 
   ngOnInit() {
+
   }
 
 }
